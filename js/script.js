ac9a6eac9c625412aed0ee3ffbe0fa145f010857
plries <apaulries@gmail.com>
2024-03-21 15:25:49 -0700

    started html markup + spotify api setup

---
+++ js/script.js
@@ -0,0 +1,19 @@
+var client_id = '7f6d942d85954739bc48030024b3a95f';
+var client_secret = '74777c2a4d45467c8c745dbd9cb7aab2';
+
+var authOptions = {
+  url: 'https://accounts.spotify.com/api/token',
+  headers: {
+    'Authorization': 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64'))
+  },
+  form: {
+    grant_type: 'client_credentials'
+  },
+  json: true
+};
+
+request.post(authOptions, function(error, response, body) {
+  if (!error && response.statusCode === 200) {
+    var token = body.access_token;
+  }
+});
