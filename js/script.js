897ce8c6c228ac8f5ada229fa5f9e35d3b00ecaf
plries <apaulries@gmail.com>
2024-03-27 19:20:40 -0700

    finished tools + responsive, started brew page

--- js/script.js
+++ js/script.js
@@ -1,12 +1,8 @@
-// nav
+// mobile nav
 
-const menu = document.getElementById("menu");
-
-function toggleMenu() {
-        
-  menu.classList.toggle("toggleMenu");
-
-};
+$("#menu").click(function(){
+  $("nav").toggleClass("toggleMenu");
+});
 
 // jQuery backgroundVideo
 $(document).ready(function(){
@@ -23,22 +19,66 @@ $(document).ready(function(){
 
 // scrollReveal
 const slideUp = {
-  distance: '125%',
-  origin: 'bottom',
+  distance: "125%",
+  origin: "bottom",
   opacity: 0,
   duration: 1000,
-  interval: 300
+  interval: 300,
 };
 
 const slideRight = {
-  distance: '125%',
-  origin: 'left',
+  distance: "125%",
+  origin: "left",
   opacity: 0,
   duration: 1000,
   delay: 350
 };
 
-ScrollReveal().reveal('.show', slideUp);
+const Reveal = {
+  scale: 0.9,
+  opacity: 0.3,
+  duration: 400,
+  interval: 300,
+  reset: true,
+  viewFactor: 1
+}
 
-ScrollReveal().reveal('#cta', slideRight);
+ScrollReveal().reveal(".show", slideUp);
+ScrollReveal().reveal("#cta", slideRight);
 
+ScrollReveal().reveal(".showTool", Reveal);
+
+// gsap
+
+gsap.set(".beans", {xPercent:-50});
+
+var rotate = gsap.timeline({
+  scrollTrigger:{
+    scrub: 0.5,
+  }
+})
+.to(".beans", {
+  rotation: 360,
+  duration: 5,
+  ease:'power1',
+})
+
+// swiper
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
