c0dfba85193d86c4338b91ce6752fd640ee509c1
plries <apaulries@gmail.com>
2024-04-07 01:19:31 -0700

    added sorting functionality + comments

--- js/isotope.js
+++ js/isotope.js
@@ -7,17 +7,19 @@ $(document).ready(function() {
     // element with ID "#cards" is assigned to variable 
     // apply following options to the variable
     var $cards = $("#cards").isotope({
-
     // defines the items in the collection
     // this allows for Isotope to properly organize the cards
     itemSelector: ".card",
     // lays out cards in a vertical column
     layoutMode: "vertical",
-    // sort cards by data-category defined in each card's attribute
     getSortData: {
-        category: "[data-category]"
+        // sort cards by data-category defined in each card's attribute
+        category: "[data-category]",
+        // sort cards by ".name" class defined in each card's attribute
+        name: ".name",
+        number: ".number"
     }
-    });
+});
 
     // when a button is clicked in the ".button-group" div...
     $(".button-group").on("click", "button", function() {
@@ -26,6 +28,14 @@ $(document).ready(function() {
         var filterValue = $(this).attr("data-filter");
         // filters the cards by the data-filter attribute
         $cards.isotope({ filter: filterValue });
-    })
+    });
 
+    // when a button is clicked in the ".sort-by-button-group" div...
+    $(".sort-by-button-group").on( "click", "button", function() {
+        // creates a variable called sortByValue
+        // assigns the data-sort-by attribute to sortByValue according to the button clicked
+        var sortByValue = $(this).attr("data-sort-by");
+        // filters the cards by the data-sort-by attribute
+        $cards.isotope({ sortBy: sortByValue });
+    });
 });
